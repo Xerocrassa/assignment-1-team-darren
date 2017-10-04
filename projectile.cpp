e6ed7f6cab2fb0e26c8f1c82537d48129fb3e973
booklover98 <slhusack@gmail.com>
2017-10-04 11:35:57 -0400

    added comments

--- projectile.cpp
+++ projectile.cpp
@@ -3,20 +3,23 @@
 const double m = 1; // in kg
 const double g = 9.8066; // no longer used
 const double dt = 0.001;
-const double k = 0.5; // in m^-1
-const double q = 1; //  in C
-const double w = 2 // in s^-1
-const double Eo = 1 // in V m^-1
+const double k = 0.5; // in m^-1 - added
+const double q = 1; //  in C - added
+const double w = 2 // in s^-1 - added
+const double Eo = 1 // in V m^-1 - added
 
+// Added
 auto electric_field(TState s) {
  double E = (Eo/sqrt(2)) 
   return E;
 }
 
 //auto force(TState s) { return VecR2<double>{0, -m * g}; }
+// Added
 auto force(TState s) {return VecR3<double>{q * E}; }
 
-auto euler_step(TState s, VecR2<double> accel) {
+// changed from VecR2
+auto euler_step(TState s, VecR3<double> accel) { 
   TState next;
   next.t = s.t + dt;
   next.position = s.position + (s.velocity * dt);
@@ -24,11 +27,13 @@ auto euler_step(TState s, VecR2<double> accel) {
   return next;
 }
 
+// added,, not sure
 auto verlet_algorithm(TState s, VecR3<double) accel) {
   TState next;
   ne
 }
 
+// not yet touched
 void n_steps(unsigned n, TState state0) {
   print_tstate(state0);
   if (n == 0)
@@ -42,7 +47,8 @@ void n_steps(unsigned n, TState state0) {
   }
 }
 
-int main() {
+// not yet touched.
+int main() { 
   n_steps(1200, TState{0., {0, 0.1}, {5, 5}, {0, 0}});
   return 0;
 }
